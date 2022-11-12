2f23e140e28d3e02992c171046ede4a7d8ee61ce
Mohamed Askar <59427434+askarthemasss@users.noreply.github.com>
2022-11-12 16:25:07 +0530

    Create RunTimePolymorphism.java

---
+++ java-t-point/OOPs/java-polymorphism/RunTimePolymorphism.java
@@ -0,0 +1,16 @@
+// Example of Java Runtime Polymorphism
+class Bike{
+   void run(){System.out.println("running");}
+ }
+ class Splender extends Bike{
+   void run(){System.out.println("running safely with 60km");}
+ 
+   public static void main(String args[]){
+     Bike b = new Splender();//upcasting
+     b.run();       // running safely with 60km
+   }
+ }
+
+
+
+// 
