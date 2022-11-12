3d500f0fd67ee8fa0ee3acf7d3696cd3c210940b
Mohamed Askar <59427434+askarthemasss@users.noreply.github.com>
2022-11-12 16:34:13 +0530

    Update RunTimePolymorphism.java

--- java-t-point/OOPs/java-polymorphism/RunTimePolymorphism.java
+++ java-t-point/OOPs/java-polymorphism/RunTimePolymorphism.java
@@ -12,5 +12,31 @@ class Bike{
  }
 
 
+// Bank - This example is also given in method overriding but there was no upcasting.
+class Bank{
+   float getRateOfInterest(){
+      return 0;
+   }
+}
 
-// 
+class SBI extends Bank{
+   float getRateOfInterest(){
+      return 8.4f;
+   }
+}
+
+class ICICI extends Bank{
+   float getRateOfInterest(){
+      return 7.3f;
+   }
+}
+
+class AXIS extends Bank{
+   float getRateOfInterest(){
+      return 9.7f;
+   }
+}
+
+class TestPolymorphism{
+   
+}
