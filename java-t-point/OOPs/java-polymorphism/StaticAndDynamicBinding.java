77804497f2d088f5b4e0a6a1861fa81626b1c0cb
Mohamed Askar <59427434+askarthemasss@users.noreply.github.com>
2022-11-15 16:14:57 +0530

    Update StaticAndDynamicBinding.java

--- java-t-point/OOPs/java-polymorphism/StaticAndDynamicBinding.java
+++ java-t-point/OOPs/java-polymorphism/StaticAndDynamicBinding.java
@@ -11,3 +11,24 @@ class Dog{
  }
 }
 
+// Example of dynamic binding
+class Animal{
+ void eat(){
+  System.out.println("animal is eating...");
+ }
+}
+
+class Dog extends Animal{
+ void eat(){
+  System.out.println("dog is eating...");
+ }
+
+ public static void main(String args[]){
+  Animal a=new Dog();
+  a.eat();        // dog is eating...
+ }
+}
+/*
+In the above example object type cannot be determined by the compiler,
+because the instance of Dog is also an instance of Animal.So compiler doesn't know its type, only its base type.
+*/
