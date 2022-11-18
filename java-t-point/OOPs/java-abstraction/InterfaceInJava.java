041919127883efbef5de2c3b4d871b74a2964116
Mohamed Askar <59427434+askarthemasss@users.noreply.github.com>
2022-11-18 16:10:36 +0530

    Create InterfaceInJava.java

---
+++ java-t-point/OOPs/java-abstraction/InterfaceInJava.java
@@ -0,0 +1,114 @@
+// In this example, the Printable interface has only one method, and its implementation is provided in the A6 class.
+interface printable{
+  void print();
+}
+
+class A6 implements printable{
+  public void print(){
+    System.out.println("Hello");
+  }
+  public static void main(String args[]){
+    A6 obj = new A6();
+    obj.print();
+  }
+}
+
+
+
+// Java Interface Example: Drawable
+
+//Interface declaration: by first user
+interface Drawable{
+  void draw();
+}
+
+//Implementation: by second user
+class Rectangle implements Drawable{
+  public void draw(){
+    System.out.println("drawing rectangle");
+  }
+}
+
+class Circle implements Drawable{
+  public void draw(){
+    System.out.println("drawing circle");
+  }
+}
+
+//Using interface: by third user
+class TestInterface1{
+  public static void main(String args[]){
+    Drawable d = new Circle();  //In real scenario, object is provided by method e.g. getDrawable()
+    d.draw();                   // drawing circle
+  }
+}
+
+
+
+// Java Interface Example: Bank
+interface Bank{
+  float rateOfInterest();
+}
+
+class SBI implements Bank{
+  public float rateOfInterest(){
+    return 9.15f;
+  }
+}
+
+class PNB implements Bank{
+  public float rateOfInterest(){
+    return 9.7f;
+  }
+}
+
+class TestInterface2{
+  public static void main(String[] args){
+    Bank b = new SBI();
+    System.out.println("ROI: "+b.rateOfInterest());   // ROI: 9.15
+  }
+}
+
+
+// Multiple inheritance in Java by interface
+interface Printable{
+  void print();
+}
+
+interface Showable{
+  void show();
+}
+
+class A7 implements Printable,Showable{
+  public void print(){
+    System.out.println("Hello");
+  }
+  public void show(){
+    System.out.println("Welcome");
+  }
+  public static void main(String args[]){
+    A7 obj = new A7();
+    obj.print();
+    obj.show();
+  }
+}
+
+
+
+// Multiple inheritance is not supported through class in java, but it is possible by an interface
+interface Printable{
+  void print();
+}
+interface Showable{
+  void print();
+}
+
+class TestInterface3 implements Printable, Showable{
+    public void print(){
+      System.out.println("Hello");
+    }
+    public static void main(String args[]){
+      TestInterface3 obj = new TestInterface3();
+      obj.print();
+    }
+}
