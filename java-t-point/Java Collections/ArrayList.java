1ddc033e21ce405cb9f16a0c6d4aafaf3a50487a
Mohamed Askar <59427434+askarthemasss@users.noreply.github.com>
2022-12-01 17:41:51 +0530

    Update ArrayList.java

--- java-t-point/Java Collections/ArrayList.java
+++ java-t-point/Java Collections/ArrayList.java
@@ -11,3 +11,43 @@ import java.util.*;
         System.out.println(list);
    }
 }
+
+
+// ArrayListExample2.java
+import java.util.*;  
+public class ArrayListExample2{  
+     public static void main(String args[]){  
+          ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
+         
+          list.add("Mango");                    //Adding object in arraylist    
+          list.add("Apple");    
+          list.add("Banana");    
+          list.add("Grapes");    
+         
+          //Traversing list through Iterator  
+          Iterator itr=list.iterator();         //getting the Iterator  
+          while(itr.hasNext()){                 //check if iterator has the elements  
+           System.out.println(itr.next());      //printing the element and move to next  
+          }  
+     }  
+}  
+
+
+// ArrayListExample3.java
+import java.util.*;  
+public class ArrayListExample3{  
+ public static void main(String args[]){  
+  ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
+     
+  list.add("Mango");//Adding object in arraylist    
+  list.add("Apple");    
+  list.add("Banana");    
+  list.add("Grapes");    
+     
+  //Traversing list through for-each loop  
+  for(String fruit:list)    
+    System.out.println(fruit);
+ }  
+}  
+
+
