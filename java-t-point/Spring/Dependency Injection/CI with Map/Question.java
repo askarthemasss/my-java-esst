42f88d23433eb530709cff915ce6f0d4cc1381f2
Mohamed Askar <59427434+askarthemasss@users.noreply.github.com>
2022-12-09 12:46:55 +0530

    Update Question.java

--- java-t-point/Spring/Dependency Injection/CI with Map/Question.java
+++ java-t-point/Spring/Dependency Injection/CI with Map/Question.java
@@ -1,4 +1,5 @@
 package com.javatpoint;
+
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -24,11 +25,12 @@ public class Question {
       System.out.println("Answers....");
     
       Set<Entry<String, String>> set = answers.entrySet();
+//       Set set = answers.entrySet();
     
       Iterator<Entry<String, String>> itr = set.iterator();
       while(itr.hasNext()){
           Entry<String,String> entry = itr.next();
-          System.out.println("Answer: "+entry.getKey()+" Posted By: "+entry.getValue());
+          System.out.println("Answer: " + entry.getKey() + " Posted By: " + entry.getValue());
       }
   }
 }
