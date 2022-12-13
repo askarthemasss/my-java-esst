f3a954d247a3d27da8843b4972ef48af262779ec
Mohamed Askar <59427434+askarthemasss@users.noreply.github.com>
2022-12-13 10:06:29 +0530

    Update HelloController.java

--- java-t-point/Spring/Spring MVC/Model Interface/HelloController.java
+++ java-t-point/Spring/Spring MVC/Model Interface/HelloController.java
@@ -11,9 +11,22 @@ public class HelloController{
   @RequestMapping('/hello')
   public String display(HttpServletRequest req, Model m){
     
-     //read the provided form data
+     //read the provided form data(index.jsp)
     String name = req.getParameter("name");
     String pass = req.getParameter("pass");
+    
+    // Validation
+    if(pass.equals("admin")){
+      String msg = "Hello " + name;
+      // Add a Message to Model
+      m.addAttribute("message", msg);
+      return "viewPage";
+    }
+    else {
+      String msg = "Hello " + name + ", you entered an incorrect password!!";
+      m.addAttribute("message",msg);
+      return "errorPage";
+    }
     
   }
 }
